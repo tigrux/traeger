127915868fa4ea852c47c533189a029ee72d84e0
Sandino Flores <tigrux@gmail.com>
2025-05-08 21:24:05 -0600

    CMake: Fetch dependencies using git

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -7,9 +7,8 @@ if(NOT Immer_FOUND)
     option(immer_BUILD_EXTRAS "Build immer extras" ON)
     FetchContent_Declare(
         immer
-        URL https://github.com/arximboldi/immer/archive/refs/tags/v0.8.1.tar.gz
-        URL_HASH
-            SHA256=de8411c84830864604bb685dc8f2e3c0dbdc40b95b2f6726092f7dcc85e75209
+        GIT_REPOSITORY https://github.com/arximboldi/immer
+        GIT_TAG v0.8.1
         DOWNLOAD_EXTRACT_TIMESTAMP ON
         OVERRIDE_FIND_PACKAGE
         EXCLUDE_FROM_ALL
@@ -24,9 +23,8 @@ if(NOT nlohmann_json_FOUND)
     option(JSON_BuildTests "Build json unit tests" OFF)
     FetchContent_Declare(
         nlohmann_json
-        URL https://github.com/nlohmann/json/archive/refs/tags/v3.12.0.tar.gz
-        URL_HASH
-            SHA256=4b92eb0c06d10683f7447ce9406cb97cd4b453be18d7279320f7b2f025c10187
+        GIT_REPOSITORY https://github.com/nlohmann/json
+        GIT_TAG v3.12.0
         DOWNLOAD_EXTRACT_TIMESTAMP ON
         OVERRIDE_FIND_PACKAGE
         EXCLUDE_FROM_ALL
@@ -42,12 +40,11 @@ if(NOT yaml-cpp_FOUND)
     option(YAML_CPP_BUILD_TESTS "Enable yaml-cpp tests" OFF)
     FetchContent_Declare(
         yaml-cpp
-        URL https://github.com/jbeder/yaml-cpp/archive/refs/tags/0.8.0.tar.gz
-        URL_HASH
-            SHA256=fbe74bbdcee21d656715688706da3c8becfd946d92cd44705cc6098bb23b3a16
+        GIT_REPOSITORY https://github.com/jbeder/yaml-cpp
+        GIT_TAG 0.8.0
         PATCH_COMMAND
-            patch -p1 <
-            ${CMAKE_CURRENT_SOURCE_DIR}/patches/yaml-cpp-include-cstdint.diff
+            git apply
+            ${CMAKE_CURRENT_SOURCE_DIR}/patches/0001-Fix-missing-header-cstdint.patch
         UPDATE_DISCONNECTED 1
         DOWNLOAD_EXTRACT_TIMESTAMP ON
         OVERRIDE_FIND_PACKAGE
@@ -64,10 +61,8 @@ if(NOT msgpack-cxx_FOUND)
     option(MSGPACK_BUILD_DOCS "Build msgpack Doxygen documentation" OFF)
     FetchContent_Declare(
         msgpack-cxx
-        URL
-            https://github.com/msgpack/msgpack-c/archive/refs/tags/cpp-7.0.0.tar.gz
-        URL_HASH
-            SHA256=070881ebea9208cf7e731fd5a46a11404025b2f260ab9527e32dfcb7c689fbfc
+        GIT_REPOSITORY https://github.com/msgpack/msgpack-c
+        GIT_TAG cpp-7.0.0
         DOWNLOAD_EXTRACT_TIMESTAMP ON
         OVERRIDE_FIND_PACKAGE
         EXCLUDE_FROM_ALL
@@ -90,9 +85,8 @@ if(NOT cppzmq_FOUND)
     set(ZMQ_BUILD_TESTS OFF CACHE BOOL "Build the tests for ZeroMQ")
     FetchContent_Declare(
         libzmq
-        URL https://github.com/zeromq/libzmq/archive/refs/tags/v4.3.5.tar.gz
-        URL_HASH
-            SHA256=6c972d1e6a91a0ecd79c3236f04cf0126f2f4dfbbad407d72b4606a7ba93f9c6
+        GIT_REPOSITORY https://github.com/zeromq/libzmq
+        GIT_TAG v4.3.5
         DOWNLOAD_EXTRACT_TIMESTAMP ON
         OVERRIDE_FIND_PACKAGE
         EXCLUDE_FROM_ALL
@@ -100,9 +94,8 @@ if(NOT cppzmq_FOUND)
     option(CPPZMQ_BUILD_TESTS "Whether or not to build the cppzmq tests" OFF)
     FetchContent_Declare(
         cppzmq
-        URL https://github.com/zeromq/cppzmq/archive/refs/tags/v4.10.0.tar.gz
-        URL_HASH
-            SHA256=c81c81bba8a7644c84932225f018b5088743a22999c6d82a2b5f5cd1e6942b74
+        GIT_REPOSITORY https://github.com/zeromq/cppzmq
+        GIT_TAG v4.10.0
         DOWNLOAD_EXTRACT_TIMESTAMP ON
         OVERRIDE_FIND_PACKAGE
         EXCLUDE_FROM_ALL
